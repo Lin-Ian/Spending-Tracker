7df7a4e2d4c74e472969b7fba01aa90e5964dd69
Ian <ianrp.lin@gmail.com>
2024-01-06 18:13:02 -0500

    Add INSERT operation for transactions in Flask application

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
 from dotenv import load_dotenv
+import psycopg2
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import DateField, StringField, DecimalField, FieldList
 from wtforms.validators import DataRequired
 
 load_dotenv()
+
+
+def get_db_connection():
+    conn = psycopg2.connect(host=os.environ['DB_HOST'],
+                            database=os.environ['DB_NAME'],
+                            user=os.environ['DB_USERNAME'],
+                            password=os.environ['DB_PASSWORD'])
+    return conn
 
 
 class Transaction(FlaskForm):
@@ -30,7 +39,18 @@ def home():
 
     transaction = Transaction()
     if transaction.validate_on_submit():
-        print("good")
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO transactions '
+                    '(date, vendor, location, subtotal, discount, tax, tip, total, payment_method, notes) '
+                    'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING transaction_id',
+                    (transaction.date.data, transaction.vendor.data, transaction.location.data,
+                     transaction.subtotal.data, transaction.discount.data, transaction.tax.data,
+                     transaction.tip.data, transaction.total.data, transaction.payment_method.data,
+                     transaction.notes.data))
+        conn.commit()
+        cur.close()
+        conn.close()
 
     return render_template('home.html', transaction=transaction)
 
