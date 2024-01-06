bd54fb6785141b14b5473d2af4954222b2897937
Ian <ianrp.lin@gmail.com>
2024-01-06 17:01:35 -0500

    Update SQL operations for transactions table for the discount column

--- test_db.py
+++ test_db.py
@@ -14,9 +14,10 @@ conn = psycopg2.connect(host=os.environ['DB_HOST'],
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
-cur.execute('INSERT INTO transactions (date, vendor, location, subtotal, tax, tip, total, payment_method, notes) '
-            'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING transaction_id',
-            ('2023-11-18', 'Shoppers Drug Mart', 'Waterloo', 12.98, 0.78, 0, 13.76, 'Tangerine Card', ''))
+cur.execute('INSERT INTO transactions '
+            '(date, vendor, location, subtotal, discount, tax, tip, total, payment_method, notes) '
+            'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING transaction_id',
+            ('2023-11-18', 'Shoppers Drug Mart', 'Waterloo', 12.98, 0, 0.78, 0, 13.76, 'Tangerine Card', ''))
 
 transaction_id = cur.fetchall()[0]
 cur.execute('INSERT INTO products '
@@ -29,9 +30,11 @@ cur.execute('INSERT INTO products '
             (transaction_id, 'Eggs', 'Food', 'Groceries', 2, 3.49, 6.98, ''))
 
 cur.execute('UPDATE transactions '
-            'SET date=%s, vendor=%s, location=%s, subtotal=%s, tax=%s, tip=%s, total=%s, payment_method=%s, notes=%s '
+            'SET date=%s, vendor=%s, location=%s, subtotal=%s, discount=%s, tax=%s, tip=%s, total=%s, '
+            'payment_method=%s, notes=%s '
             'WHERE transaction_id=%s',
-            ('2023-11-18', 'Shoppers Drug Mart', 'Waterloo', 12.98, 0.78, 0, 13.76, 'Tangerine Card', 'Sample Comment',
+            ('2023-11-18', 'Shoppers Drug Mart', 'Waterloo', 12.98, 0, 0.78, 0, 13.76,
+             'Tangerine Card', 'Sample Comment',
              transaction_id))
 
 cur.execute('UPDATE products '
