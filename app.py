f6feb05d2608c7200320d04d1ede4bac7df00044
Ian <ianrp.lin@gmail.com>
2024-01-06 18:01:34 -0500

    Create form for transaction information

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import DateField, StringField, DecimalField, FieldList
+from wtforms.validators import DataRequired
+
+load_dotenv()
+
+
+class Transaction(FlaskForm):
+    date = DateField("Date", validators=[DataRequired()])
+    vendor = StringField("Vendor", validators=[DataRequired()])
+    location = StringField("Location", validators=[DataRequired()])
+    subtotal = DecimalField("Subtotal", validators=[DataRequired()])
+    discount = DecimalField("Discount", validators=[DataRequired()])
+    tax = DecimalField("Tax", validators=[DataRequired()])
+    tip = DecimalField("Tip", validators=[DataRequired()])
+    total = DecimalField("Total", validators=[DataRequired()])
+    payment_method = StringField("Payment Method", validators=[DataRequired()])
+    notes = StringField("Notes")
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+
+    transaction = Transaction()
+    if transaction.validate_on_submit():
+        print("good")
+
+    return render_template('home.html', transaction=transaction)
 
 
 if __name__ == "__main__":
